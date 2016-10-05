37e581c152067ddaba2624625d939ca65b904c6a
Shisha <shisha225@gmail.com>
2016-10-05 15:55:03 +0800

    下載功能

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,11 +1,15 @@
 require('./libs/html2canvas-0.4.1/html2canvas.min.js');
 
-console.log(html2canvas);
-
 $(document).ready(function() {
   $("input[type=text], input[type=number]").focus(function() {
     $(this).select();
   });
+
+
+
+  // ===========================================================================
+  // = 處理標題輸入
+  // ===========================================================================
 
   var $inputTitle = $('.input_title');
   var $inputTitleExtra = $('.input_title_extra');
@@ -14,7 +18,7 @@ $(document).ready(function() {
   $inputTitleExtra.attr('maxlength', 9).attr('placeholder', '這邊可以輸入九個字');
 
   // 統一標題
-  $('#section_title_inject').ready(() => {
+  $('#section_title_inject').ready(function() {
     $(this).find('button').click((event) => {
       var title = $(this).find('.input_title').val();
       var titleExtra = $(this).find('.input_title_extra').val();
@@ -22,5 +26,35 @@ $(document).ready(function() {
       $inputTitle.val(title);
       $inputTitleExtra.val(titleExtra);
     });
-  })
+  });
+
+  // ===========================================================================
+  // = 處理儲存輸出
+  // ===========================================================================
+
+  var $buttonOutput = $('#button_output');
+
+  $buttonOutput.attr('disabled', true);
+
+  $('form').submit(function() {
+    $buttonOutput.attr('disabled', false);
+  });
+
+  $buttonOutput.click(function() {
+    console.log('輸出');
+    html2canvas(this, {
+      onrendered: (canvas) => {
+        var dataUrl = canvas.toDataURL('image/jpeg');
+        var link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = '100x100';
+        link.dispatchEvent(new MouseEvent('click', {
+          view: window,
+          bubbles: true,
+          cancelable: true
+        }));
+        console.log('fin');
+      }
+    });
+  });
 });
