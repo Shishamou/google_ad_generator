d84e98acd3185ded72034a102f4c3f79a4faa617
Shisha <shisha225@gmail.com>
2016-10-06 15:32:26 +0800

    編輯 js 註解

--- resources/assets/js/print.js
+++ resources/assets/js/print.js
@@ -3,17 +3,24 @@ window.$ = window.jQuery = require('jquery');
 
 $(document).ready(function() {
   $('.box').map(function() {
-    // 替換圖片
+
+    // =========================================================================
+    // = 替換圖片
+    // =========================================================================
+    // 將區塊內商品圖片替換為 input[name=image] 所指定 dataurl
+
     $(this).find('.pic img').map(function() {
       var imageUrl = $('input[name=image]').val();
       if (imageUrl) {
         $(this).attr('src', imageUrl);
       }
-
-      $(this).attr('alt', $('input[name=uniqid]').val());
     });
 
-    // 區塊轉換為圖片
+    // =========================================================================
+    // = 區塊轉換為圖片
+    // =========================================================================
+    // 將 .box 區塊透過 html2canvas 套件轉換為圖片
+
     if ( ! $('input[name=testing]').val()) {
       html2canvas(this, {
         onrendered: (canvas) => {
