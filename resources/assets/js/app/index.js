6625f8877c3627ba5b984ab40b08d32259675c6a
Shisha <shisha225@gmail.com>
2016-10-07 19:59:24 +0800

    填入資料功能完成

--- resources/assets/js/app/index.js
+++ resources/assets/js/app/index.js
@@ -2,12 +2,14 @@ const Vue = require('vue');
 import methods from './methods';
 
 const data = {
-  showToolbar: false,
+  showToolbar: true,
   shouldAutoDraw: true,
   inputUrl: '',
   image: '',
   price: '',
   sale: '',
+  title: '',
+  titleExtra: '',
 };
 
 module.exports = new Vue({ data, methods });
--- resources/assets/js/app/methods.js
+++ resources/assets/js/app/methods.js
@@ -1,11 +1,19 @@
 window.$ = window.jQuery = require('jquery');
 
+const seed = {
+  title: '英國凱旋機車',
+  titleExtra: '恆久榮耀 品味不凡',
+  price: 638000,
+  sale: 458000,
+  image: 'image/seed.jpg',
+};
+
 export default {
   /**
    * 注入種子
    */
   doInjectSeed: function() {
-
+    Object.assign(this, seed);
   },
 
   /**
@@ -34,5 +42,13 @@ export default {
    */
   handleInputFile: function() {
     console.log('file');
+  },
+
+  /**
+   *
+   */
+  doSyncTitleText: function() {
+    $('.input_title').val(this.inputTitle);
+    $('.input_title_extra').val(this.inputTitleExtra);
   },
 }
